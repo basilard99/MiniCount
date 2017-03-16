595785b9f23f4b2405bc9d759c9c41f18b33bcce
Chris Snyder <Chris Snyder>
2017-03-16 00:17:27 -0400

    Initial development

---
+++ MiniCount/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiniCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
